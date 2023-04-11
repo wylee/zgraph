3482031d08d9ff3b169b35b34ac02260640bf73f
Wyatt Baldwin <self@wyattbaldwin.com>
2023-04-10 22:39:54 -0700

    WIP

--- src/graph.zig
+++ src/graph.zig
@@ -1,81 +1,71 @@
 const std = @import("std");
 
-/// Make an arena allocator for use with a typical graph.
-pub fn make_arena() std.heap.ArenaAllocator {
-    return std.heap.ArenaAllocator.init(std.heap.page_allocator);
-}
-
-/// Create a new graph type with the specified node type.
-pub fn GraphType(comptime NodeType: type) type {
-    const Node = struct {
+fn NodeType(comptime NodeValue: type) type {
+    return struct {
         const Self = @This();
 
-        value: NodeType,
+        value: NodeValue,
         neighbors: std.ArrayList(*Self),
 
-        fn new(allocator: std.mem.Allocator, value: NodeType) Self {
-            return Self{
+        fn new(allocator: *std.mem.Allocator, value: NodeValue) Self {
+            return .{
                 .value = value,
-                .neighbors = std.ArrayList(*Self).init(allocator),
+                .neighbors = std.ArrayList(*Self).init(allocator.*),
             };
         }
 
-        fn add_neighbor(self: *Self, neighbor: *Self) !void {
-            try self.neighbors.append(neighbor);
+        fn add_neighbor(self: *Self, node: *Self) !void {
+            try self.neighbors.append(node);
         }
 
-        fn neighbor_count(self: Self) usize {
+        fn neighbor_count(self: *const Self) usize {
             return self.neighbors.items.len;
         }
     };
+}
 
-    const Graph = struct {
+/// Create a new graph type with the specified node and edge value
+/// types.
+pub fn GraphType(comptime NodeValue: type) type {
+    const Node = NodeType(NodeValue);
+
+    return struct {
         const Self = @This();
 
-        allocator: std.mem.Allocator,
-        nodes: std.ArrayList(Node),
+        allocator: *std.mem.Allocator,
+        nodes: std.ArrayList(*Node),
 
-        pub fn new(allocator: std.mem.Allocator) Self {
-            return Self{
+        pub fn new(allocator: *std.mem.Allocator) Self {
+            return .{
                 .allocator = allocator,
-                .nodes = std.ArrayList(Node).init(allocator),
+                .nodes = std.ArrayList(*Node).init(allocator.*),
             };
         }
 
-        pub fn add_node(self: *Self, value: NodeType) !Node {
-            const node = Node.new(self.allocator, value);
+        pub fn add_node(self: *Self, value: NodeValue) !*Node {
+            var node = try self.nodes.allocator.create(Node);
+            node.value = value;
             try self.nodes.append(node);
-            return node;
+            return self.nodes.getLast();
         }
 
         pub fn add_edge(_: Self, u: *Node, v: *Node) !void {
-            std.log.warn("\n\n", .{});
-            std.log.warn("BEGIN add_edge", .{});
-            std.log.warn("neighbor count: {}", .{u.neighbors.items.len});
             try u.add_neighbor(v);
-            std.log.warn("neighbor count: {}", .{u.neighbors.items.len});
-            std.log.warn("neighbors[0] value: {}", .{u.neighbors.items[0].value});
-            std.log.warn("END add_edge", .{});
         }
 
         pub fn node_count(self: Self) usize {
             return self.nodes.items.len;
         }
 
-        pub fn edge_count(self: Self) usize {
-            std.log.warn("\n\n", .{});
-            std.log.warn("BEGIN edge_count", .{});
+        pub fn edge_count(self: *const Self) usize {
             var count: usize = 0;
             for (self.nodes.items) |node| {
-                std.log.warn("node value: {}", .{node.value});
-                std.log.warn("neighbor count: {}", .{node.neighbor_count()});
+                std.log.warn("{}", .{@TypeOf(node)});
+                std.log.warn("n count for {} = {}", .{ node.value, node.neighbor_count() });
                 count += node.neighbor_count();
-                std.log.warn("total count: {}", .{count});
             }
-            std.log.warn("END edge_count", .{});
+            std.log.warn("TOTAL = {}", .{count});
             return count;
         }
     };
-
-    return Graph;
 }
--- src/test_graph.zig
+++ src/test_graph.zig
@@ -1,26 +1,29 @@
 const std = @import("std");
 const expect = std.testing.expect;
-const graphNS = @import("graph.zig");
+const GraphType = @import("graph.zig").GraphType;
 
 test "basic graph construction" {
-    const Graph_I32 = graphNS.GraphType(i32);
+    const Graph = GraphType(i32);
 
-    var arena = graphNS.make_arena();
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    var allocator = arena.allocator();
     defer arena.deinit();
 
-    var graph = Graph_I32.new(arena.allocator());
+    var graph = Graph.new(&allocator);
     try expect(graph.node_count() == 0);
     // try expect(graph.edge_count() == 0);
 
     var u = try graph.add_node(1);
     try expect(graph.node_count() == 1);
+    std.log.warn("added node with value = {}", .{u.value});
     // try expect(graph.edge_count() == 0);
 
     var v = try graph.add_node(2);
     try expect(graph.node_count() == 2);
     // try expect(graph.edge_count() == 0);
 
-    try graph.add_edge(&u, &v);
+    try graph.add_edge(u, v);
+    try expect(graph.node_count() == 2);
     try expect(graph.edge_count() == 1);
 
     // try graph.add_edge(&v, &u);
